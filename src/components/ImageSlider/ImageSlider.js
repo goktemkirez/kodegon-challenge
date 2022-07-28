72b8afb22e0927faf065d6bafadc6b3861bbbbc7
goktemkirez <ogkirez@gmail.com>
2022-07-28 12:22:40 +0300

    Image Slider added

---
+++ src/components/ImageSlider/ImageSlider.js
@@ -0,0 +1,35 @@
+import React from "react";
+import SimpleImageSlider from "react-simple-image-slider";
+import { Box } from "@mui/material";
+import useMediaQuery from "@mui/material/useMediaQuery";
+
+import { useStyles } from "./ImageSlider.style";
+
+const images = [
+  { url: "https://natureconservancy-h.assetsadobe.com/is/image/content/dam/tnc/nature/en/photos/Zugpsitze_mountain.jpg?crop=0%2C176%2C3008%2C1654&wid=4000&hei=2200&scl=0.752" },
+  { url: "https://scx2.b-cdn.net/gfx/news/2019/2-nature.jpg" },
+  { url: "https://natureconservancy-h.assetsadobe.com/is/image/content/dam/tnc/nature/en/photos/tnc_16935516.jpg?crop=0%2C235%2C4000%2C2200&wid=4000&hei=2200&scl=1.0" },
+];
+
+const ImageSlider = (props) => {
+  const classes = useStyles();
+  const isMobile = useMediaQuery('(max-width:600px)');
+
+  return <Box className={classes.imageSliderDiv}>
+    {isMobile ? <SimpleImageSlider
+      width="100%"
+      height={450}
+      images={images}
+      showBullets={true}
+      showNavs={true}
+    /> : <SimpleImageSlider
+      width="100%"
+      height={600}
+      images={images}
+      showBullets={true}
+      showNavs={true}
+    />}
+  </Box>;
+};
+
+export default ImageSlider;
---
+++ src/pages/Main/Main.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import ImageSlider from "../../components/ImageSlider/ImageSlider";
+
+export default function Main() {
+    return(
+        <ImageSlider/>
+    );
+}
